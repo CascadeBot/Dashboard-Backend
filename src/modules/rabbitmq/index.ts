85b67ab9c94e2aae8d539e65d818860e2d2dc861
Jelle van Snik <jellevs@gmail.com>
2022-10-16 23:06:36 +0200

    fuck

--- src/modules/rabbitmq/index.ts
+++ src/modules/rabbitmq/index.ts
@@ -10,7 +10,7 @@ let channel: Channel | null = null;
 let callbackQueue: string | null = null;
 
 export async function setupRabbitMQ() {
-  log.info(`Connecting to rabbitMQ...`, { evt: 'connect' });
+  log.info(`Connecting to RabbitMQ...`, { evt: 'connect' });
   connection = await connect(config.rabbitmq.url);
   channel = await connection.createChannel();
 
